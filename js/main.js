aafe49484f74afa2142bf73d14612f903ee0ca3a
MichyLopez <michelynnelopez60@gmail.com>
2025-05-09 01:28:04 -0500

    ready to publish :)

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const fullMenu = document.getElementById('full-menu');
+
+const showBtn = document.getElementById('show-menu');
+
+const closeBtn = document.getElementById('close-btn');
+
+
+showBtn.addEventListener('click', function() {
+    fullMenu.classList.add('show');
+});
+
+closeBtn.addEventListener('click', function(){
+    fullMenu.classList.remove('show');
+})
